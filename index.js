a177069565e316f899cb85e44faa98e6d867efa9
Gabriel Mädke Santini <97846035+gabrielmadkesantini@users.noreply.github.com>
2023-06-23 09:20:05 -0300

    user controller finihed

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const port = 3000;
 const bodyParser = require("body-parser");
 
 const authorRouter = require("./routes/authors");
-const publish_companyRouter = require("./routes/publish_company");
 const genreRoutes = require("./routes/genre");
 const bookRoutes = require("./routes/book");
+const userRoutes = require("./routes/users");
 
 app.use(bodyParser.json(0));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(genreRoutes);
 app.use(authorRouter);
-app.use(publish_companyRouter);
 app.use(bookRoutes);
+app.use(userRoutes);
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
