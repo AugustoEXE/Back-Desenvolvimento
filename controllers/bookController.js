ea804e5c6d9c7f22d7b30c4cf747273284940239
Lagostacio <inaciobortolaslago@gmail.com>
2023-07-28 17:03:57 -0300

    books search working

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,21 +2,27 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 exports.list = async (params) => {
+    const releaseDate = !params.release_date
+        ? undefined
+        : new Date(params.release_date)
+
     return await prisma.book.findMany({
         where: {
-            name: { contains: params.name, mode: "insensitive" },
-            release_date: { equals: new Data(params.release_date) },
+            name: { contains: params.name, },
+            release_date: {
+                lte: releaseDate
+            },
             language: params.language,
             author: {
-                name: { constains: params.author, mode: "insensitive" },
+                name: { constains: params.author },
             },
             genre: {
-                name: { constains: params.genre, mode: "insensitive" },
+                name: { constains: params.genre, },
             },
             publish_company: {
                 name: {
                     constains: params.publish_company,
-                    mode: "insensitive",
+
                 },
             },
         },
