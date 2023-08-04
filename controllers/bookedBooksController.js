907b3e1f70c81a624f0289c5bffe26fb811c3ec5
Gabriel Mädke Santini <97846035+gabrielmadkesantini@users.noreply.github.com>
2023-08-04 16:02:26 -0300

    necessary commit

---
+++ controllers/bookedBooksController.js
@@ -0,0 +1,14 @@
+const { PrismaClient } = require("@prisma/client");
+const { create } = require("./userController");
+
+const prisma = new PrismaClient()
+
+exponrt.create = async (data)=> prisma.bookedBooks.create({data})
+
+exponrt.selectBookedBooks = async (id)=> prisma.bookedBooks.({})
+
+exponrt.create = async (data)=> prisma.bookedBooks.create({})
+
+exponrt.create = async (data)=> prisma.bookedBooks.create({})
+
+exponrt.create = async (data)=> prisma.bookedBooks.create({})
